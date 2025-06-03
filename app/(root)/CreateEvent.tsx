f6c8a87c0b513c4c3a0003a25813ffb8aa5cb91b
julianojosoa13 <josoajuliano@gmail.com>
2025-06-03 12:14:34 +0300

    UI: Create Event screen date section

--- app/(root)/CreateEvent.tsx
+++ app/(root)/CreateEvent.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/shared/Button";
 import { Header } from "@/components/shared/Header";
 import { SafeAreaView, ScrollView, Text, TextInput, View } from "react-native";
-import Entypo from "@expo/vector-icons/Entypo";
-import { FontAwesome6 } from "@expo/vector-icons";
+import { MaterialIcons, Entypo, AntDesign } from "@expo/vector-icons";
 import Ripple from "react-native-material-ripple";
+
+const bgColor = "#EFEFEF";
+const fgColor = "#828282";
+
 const CreateEvent = () => {
   return (
     <SafeAreaView className="bg-primary flex-1">
@@ -13,22 +15,106 @@ const CreateEvent = () => {
         className="px-4 text-2xl font-cabin-bold"
       />
       <ScrollView
-        className="flex-1 bg-white rounded-t-3xl mt-2 pt-4"
+        className="flex-1 bg-white rounded-t-3xl mt-2 pt-4 px-4"
         showsVerticalScrollIndicator={false}
       >
         <Ripple
-          className={"bg-[#EFEFEF] mx-4 px-4 h-12 justify-center rounded-full"}
+          className={`bg-[${bgColor}] px-4 h-12 justify-center rounded-full overflow-hidden`}
         >
-          <View className="flex flex-row justify-between">
+          <View className="flex flex-row justify-between items-center bg-[#EFEFEF] flex-1">
             <View className="flex-row items-center gap-2">
-              <Entypo name="star-outlined" size={20} color={"#828282"} />
+              <AntDesign name="staro" size={20} color={fgColor} />
               <Text className="font-cabin text-[#828282]">
                 Type d{"'"}évènement
               </Text>
             </View>
-            <Entypo name="chevron-thin-right" size={20} color="#828282" />
+            <Entypo name="chevron-thin-right" size={20} color={fgColor} />
           </View>
         </Ripple>
+
+        <Text className="my-4 font-cabin-bold">{"Date de l'évenement"}</Text>
+
+        <View className="flex-row items-center justify-between gap-5">
+          <Ripple
+            className={`bg-[${bgColor}] flex-1 rounded-2xl p-2 overflow-hidden`}
+          >
+            <View className="flex-row items-center gap-2">
+              <MaterialIcons name="edit-calendar" size={20} color={fgColor} />
+              <Text className={`text-[${bgColor}] font-cabin text-xs`}>
+                Début
+              </Text>
+            </View>
+
+            <View className="flex-row items-center gap-4 mb-2">
+              <Text className={`text-[40px] text-[${fgColor}] font-cabin-bold`}>
+                01
+              </Text>
+              <View>
+                <Text className={`text-[${fgColor}] font-cabin text-[16px]`}>
+                  Déc
+                </Text>
+                <Text
+                  className={`text-[${fgColor}] text-[16px] font-cabin-semibold`}
+                >
+                  2025
+                </Text>
+              </View>
+            </View>
+
+            <View className="flex-row items-center justify-between">
+              <View className="flex-row items-center gap-2">
+                <AntDesign name="clockcircleo" size={20} color={fgColor} />
+                <Text className={`text-[${fgColor}] font-cabin-semibold`}>
+                  22h
+                </Text>
+              </View>
+
+              <Entypo name="chevron-thin-right" size={20} color={fgColor} />
+            </View>
+          </Ripple>
+
+          <Ripple
+            className={`bg-[${bgColor}] flex-1 rounded-2xl p-2 overflow-hidden`}
+          >
+            <View className="flex-row items-center gap-2">
+              <MaterialIcons name="edit-calendar" size={20} color={fgColor} />
+              <Text className={`text-[${bgColor}] font-cabin text-xs`}>
+                Fin
+              </Text>
+            </View>
+
+            <View className="flex-row items-center gap-4 mb-2">
+              <Text className={`text-[40px] text-[${fgColor}] font-cabin-bold`}>
+                01
+              </Text>
+              <View>
+                <Text className={`text-[${fgColor}] font-cabin text-[16px]`}>
+                  Déc
+                </Text>
+                <Text
+                  className={`text-[${fgColor}] text-[16px] font-cabin-semibold`}
+                >
+                  2025
+                </Text>
+              </View>
+            </View>
+
+            <View className="flex-row items-center justify-between">
+              <View className="flex-row items-center gap-2">
+                <AntDesign name="clockcircleo" size={20} color={fgColor} />
+                <Text className={`text-[${fgColor}] font-cabin-semibold`}>
+                  22h
+                </Text>
+              </View>
+
+              <Entypo name="chevron-thin-right" size={20} color={fgColor} />
+            </View>
+          </Ripple>
+        </View>
+
+        <Text className="my-4 font-cabin-bold">
+          {"Ouverture de l'évenement"}
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
