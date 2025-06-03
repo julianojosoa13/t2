3b869d3717b79c9b0031019966a8d789152f6d51
julianojosoa13 <josoajuliano@gmail.com>
2025-06-03 13:06:37 +0300

    UI: Create Event screen design Done

--- app/(root)/CreateEvent.tsx
+++ app/(root)/CreateEvent.tsx
@@ -1,6 +1,18 @@
 import { Header } from "@/components/shared/Header";
-import { SafeAreaView, ScrollView, Text, TextInput, View } from "react-native";
-import { MaterialIcons, Entypo, AntDesign } from "@expo/vector-icons";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import {
+  MaterialIcons,
+  Entypo,
+  AntDesign,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import Ripple from "react-native-material-ripple";
 
 const bgColor = "#EFEFEF";
@@ -10,13 +22,16 @@ const CreateEvent = () => {
   return (
     <SafeAreaView className="bg-primary flex-1">
       <Header title="" backTo="Onboarding" />
+
       <TextInput
         placeholder="Nom de l'évenement"
         className="px-4 text-2xl font-cabin-bold"
+        multiline
       />
       <ScrollView
         className="flex-1 bg-white rounded-t-3xl mt-2 pt-4 px-4"
         showsVerticalScrollIndicator={false}
+        contentContainerClassName="flex flex-1"
       >
         <Ripple
           className={`bg-[${bgColor}] px-4 h-12 justify-center rounded-full overflow-hidden`}
@@ -63,7 +78,7 @@ const CreateEvent = () => {
 
             <View className="flex-row items-center justify-between">
               <View className="flex-row items-center gap-2">
-                <AntDesign name="clockcircleo" size={20} color={fgColor} />
+                <AntDesign name="clockcircleo" size={18} color={fgColor} />
                 <Text className={`text-[${fgColor}] font-cabin-semibold`}>
                   22h
                 </Text>
@@ -101,7 +116,7 @@ const CreateEvent = () => {
 
             <View className="flex-row items-center justify-between">
               <View className="flex-row items-center gap-2">
-                <AntDesign name="clockcircleo" size={20} color={fgColor} />
+                <AntDesign name="clockcircleo" size={18} color={fgColor} />
                 <Text className={`text-[${fgColor}] font-cabin-semibold`}>
                   22h
                 </Text>
@@ -115,6 +130,64 @@ const CreateEvent = () => {
         <Text className="my-4 font-cabin-bold">
           {"Ouverture de l'évenement"}
         </Text>
+
+        <View className="flex-row items-center justify-between gap-5">
+          <Ripple
+            className={`bg-primary-100 w-2/5 h-12 overflow-hidden rounded-xl justify-center`}
+          >
+            <View className="flex-row items-center px-4 gap-2">
+              <AntDesign name="check" color="#2C7D4E" size={18} />
+              <Text
+                className={`font-nunito-semibold text-[16px] text-primary-300`}
+              >
+                Public
+              </Text>
+            </View>
+          </Ripple>
+          <Ripple
+            className={`bg-[${bgColor}] w-2/5 h-12 overflow-hidden rounded-xl justify-center`}
+          >
+            <View className="flex-row items-center px-4 gap-2">
+              <View />
+              <Text
+                className={`font-nunito-semibold text-[16px] text-[${fgColor}]`}
+              >
+                Privé
+              </Text>
+            </View>
+          </Ripple>
+        </View>
+
+        <Text className="my-4 font-cabin-bold">{"Adresse et emplacement"}</Text>
+
+        <Ripple
+          className={`bg-[${bgColor}] px-4 h-12 justify-center rounded-full overflow-hidden`}
+        >
+          <View className="flex flex-row justify-between items-center bg-[#EFEFEF] flex-1">
+            <View className="flex-row items-center gap-2">
+              <MaterialCommunityIcons
+                name="map-marker-check-outline"
+                size={22}
+                color={fgColor}
+              />
+
+              <Text className="font-cabin text-[#828282]">
+                Ajouter une adresse
+              </Text>
+            </View>
+
+            <Entypo name="chevron-thin-right" size={20} color={fgColor} />
+          </View>
+        </Ripple>
+        <Image
+          source={require("@/assets/images/createEventBg.png")}
+          className="absolute bottom-0 -left-10 -z-10 w-full h-[200px]"
+        />
+        <Ripple
+          className={`bg-primary w-16 h-16 rounded-full items-center justify-center absolute bottom-0 right-0 mb-14 mr-5 overflow-hidden`}
+        >
+          <Entypo name="chevron-thin-right" size={20} color={"#303338"} />
+        </Ripple>
       </ScrollView>
     </SafeAreaView>
   );
